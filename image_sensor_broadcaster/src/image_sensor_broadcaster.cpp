3e97a5b287b544bf3f0db2e23078978981a86581
m12watanabe1a <40206149+m12watanabe1a@users.noreply.github.com>
2023-06-20 13:02:21 +0900

    add this pointer

--- image_sensor_broadcaster/src/image_sensor_broadcaster.cpp
+++ image_sensor_broadcaster/src/image_sensor_broadcaster.cpp
@@ -19,11 +19,12 @@ namespace image_sensor_broadcaster
 controller_interface::CallbackReturn ImageSensorBroadcaster::on_init()
 {
   try {
-    param_listener_ = std::make_shared<ParamListener>(get_node());
-    params_ = param_listener_->get_params();
+    this->param_listener_ = std::make_shared<ParamListener>(this->get_node());
+    this->params_ = this->param_listener_->get_params();
   } catch (const std::exception & e) {
     RCLCPP_ERROR(
-      get_node()->get_logger(), "Exception thrown during init stage with message: %s \n", e.what());
+      this->get_node()->get_logger(), "Exception thrown during init stage with message: %s \n",
+      e.what());
     return CallbackReturn::ERROR;
   }
 
@@ -33,32 +34,33 @@ controller_interface::CallbackReturn ImageSensorBroadcaster::on_init()
 controller_interface::CallbackReturn ImageSensorBroadcaster::on_configure(
   const rclcpp_lifecycle::State &)
 {
-  params_ = param_listener_->get_params();
+  this->params_ = this->param_listener_->get_params();
 
   try {
     // register ft sensor data publisher
     this->sensor_state_publisher_ =
       get_node()->create_publisher<sensor_msgs::msg::Image>("~/image", rclcpp::SensorDataQoS());
-    this->realtime_publisher_ = std::make_unique<StatePublisher>(sensor_state_publisher_);
+    this->realtime_publisher_ = std::make_unique<StatePublisher>(this->sensor_state_publisher_);
   } catch (const std::exception & e) {
-    fprintf(
-      stderr, "Exception thrown during publisher creation at configure stage with message : %s \n",
+    RCLCPP_ERROR(
+      this->get_node()->get_logger(),
+      "Exception thrown during publisher creation at configure stage with message : %s \n",
       e.what());
     return CallbackReturn::ERROR;
   }
 
-  realtime_publisher_->lock();
+  this->realtime_publisher_->lock();
 
-  realtime_publisher_->msg_.header.frame_id = this->params_.frame_id;
-  realtime_publisher_->msg_.encoding = this->params_.encoding;
-  realtime_publisher_->msg_.is_bigendian = static_cast<uint8_t>(this->params_.is_bigendian);
-  realtime_publisher_->msg_.height = static_cast<uint32_t>(this->params_.height);
-  realtime_publisher_->msg_.width = static_cast<uint32_t>(this->params_.width);
-  realtime_publisher_->msg_.step =
+  this->realtime_publisher_->msg_.header.frame_id = this->params_.frame_id;
+  this->realtime_publisher_->msg_.encoding = this->params_.encoding;
+  this->realtime_publisher_->msg_.is_bigendian = static_cast<uint8_t>(this->params_.is_bigendian);
+  this->realtime_publisher_->msg_.height = static_cast<uint32_t>(this->params_.height);
+  this->realtime_publisher_->msg_.width = static_cast<uint32_t>(this->params_.width);
+  this->realtime_publisher_->msg_.step =
     static_cast<uint32_t>(this->params_.width *
     sensor_msgs::image_encodings::numChannels(this->params_.encoding));
 
-  realtime_publisher_->msg_.data.reserve(
+  this->realtime_publisher_->msg_.data.reserve(
     realtime_publisher_->msg_.step * realtime_publisher_->msg_.height);
 
   realtime_publisher_->unlock();
@@ -99,10 +101,10 @@ controller_interface::CallbackReturn ImageSensorBroadcaster::on_deactivate(
 controller_interface::return_type ImageSensorBroadcaster::update(
   const rclcpp::Time & time, const rclcpp::Duration & /*period*/)
 {
-  if (realtime_publisher_ && realtime_publisher_->trylock()) {
-    realtime_publisher_->msg_.header.set__stamp(time);
+  if (this->realtime_publisher_ && this->realtime_publisher_->trylock()) {
+    this->realtime_publisher_->msg_.header.set__stamp(time);
     // realtime_publisher_->msg_.set__data();
-    realtime_publisher_->unlockAndPublish();
+    this->realtime_publisher_->unlockAndPublish();
   }
 
   return controller_interface::return_type::OK;
